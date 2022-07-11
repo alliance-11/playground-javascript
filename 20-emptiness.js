bfab7655225434458e60b78556533235373ed0c1
losrobbos <losrobbos@web.de>
2022-07-11 15:27:51 +0200

    Added emptiness sample

---
+++ 20-emptiness.js
@@ -0,0 +1,48 @@
+let str = ""
+if(!str) {
+  console.log("str has content")
+}
+
+let num = 0
+if(!num) {
+  console.log("num has content")
+}
+
+
+let obj = {} // NOT empty!!!!! WARNING!
+
+if(!obj) {
+  console.log("Obj is undefined")
+}
+else {
+  console.log("obj variable has content")
+}
+
+
+let arr = [] // NOT empty!!!!! WARNING!
+
+if(!arr) {
+  console.log("arr is undefined")
+}
+else {
+  console.log("arr has content")
+}
+
+let arrUndefined // undefined
+if(!arrUndefined) {
+  console.log("arrUndefined is undefined")
+}
+
+
+
+const nully = null
+
+if(!nully) {
+  console.log("nully has no content")
+}
+
+const undefy = undefined // const undefy
+
+if(!undefy) {
+  console.log("undefy has no content")
+}
