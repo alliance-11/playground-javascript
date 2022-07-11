ac416db58b27bb9ade73b30e1cb17e68fa28aae3
losrobbos <losrobbos@web.de>
2022-07-11 15:35:11 +0200

    Polishing

--- 20-emptiness.js
+++ 20-emptiness.js
@@ -12,27 +12,26 @@ if(!num) {
 let obj = {} // NOT empty!!!!! WARNING!
 
 if(!obj) {
-  console.log("Obj is undefined")
+  console.log("obj has content")
 }
 else {
-  console.log("obj variable has content")
+  console.log("obj has no content")
 }
 
 
 let arr = [] // NOT empty!!!!! WARNING!
 
 if(!arr) {
-  console.log("arr is undefined")
+  console.log("arr has content")
 }
 else {
-  console.log("arr has content")
+  console.log("arr has no content")
 }
 
 let arrUndefined // undefined
 if(!arrUndefined) {
-  console.log("arrUndefined is undefined")
+  console.log("arrUndefined has no content")
 }
-
 
 
 const nully = null
