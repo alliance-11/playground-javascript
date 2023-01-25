6b87dc9dfd39106a5871eefc4264ddc44e9b7a9f
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-25 12:26:59 +0100

    Robs - update

--- 22-object-copy-update-workout.js
+++ 22-object-copy-update-workout.js
@@ -3,21 +3,19 @@ const user = {
   title: "Senior TS Dev soon",
   yearsCoding: 2,
   isAdmin: false,
-  friends: ["Gael", "Rob", "Niko", "Gamze"],
-};
-console.log({ user });
-// let's manipulate the object above.
-//By creating a copy of "user" on each update...
-//and store te updated copy in variable userCopy!
+  friends: [
+    "Gael", 
+    "Rob",
+    "Niko",
+    "Gamze",
+  ]
+}
+
+// let's manipulate the object above. By creating a copy of "user" on each update... and store te updated copy in variable userCopy!
 // after each operation: console.log userCopy
-let userCopy = user;
-console.log({ userCopy });
+let userCopy
 
-// TODO: Increase years coding in object by 2! Do so by creating a copy of user, updating the field "yearsCoding" by years
-
-let increase = { ...user, yearsCoding: 4 };
-console.log({ increase });
-console.log({ user });
+// TODO: Increase years coding in object by 2! Do so by creating a copy of user. and updating the field "yearsCoding" by years
 // result:
 /**
 {
@@ -33,13 +31,9 @@ console.log({ user });
   ]
 } 
 */
-// TODO: Write a function "addYearsCoding" that receives "years" as a parameter. years is a number
+
+// TODO: Write a function "addYearsCoding" that receives "years" as a parameter. years is a number 
 // The function should create a copy of user variable, and then update the field "yearsCoding" on the copy, adding "years" to it
-let grownedExperience = {
-  ...user, yearsCoding: user.yearsCoding + 5
-}
-console.log({ grownedExperience });
-console.log({ user });
 
 // Example: userCopy = addYearsCoding(5)
 // should return back:
@@ -56,19 +50,15 @@ console.log({ user });
     "Gamze",
   ]
 } 
+
 */
 
+
 // TODO: Write a function "makeTitleUppercase" that receives no parameter.
-// The function should create a copy of user variable, and then update the field "title" on the copy, making it uppercase.
+// The function should create a copy of user variable, and then update the field "title" on the copy, making it uppercase. 
 // The result should get stored in userCopy variable
+
 // Example: userCopy = makeTitleUppercase()
-
-let makeTitleUppercase = {
-  ...user, title: user.title.toUpperCase()
-}
-console.log({ makeTitleUppercase });
-console.log({ user });
-
 // should return back:
 /**
 {
@@ -83,18 +73,14 @@ console.log({ user });
     "Gamze",
   ]
 } 
-*/
+*/ 
+
+
 
 // TODO: Write a function "addFriend" that receives "friend" as a parameter. friend is a string
 // The function should create a copy of user variable, and then update the array field "friends" on the copy, adding the new friend to it.
 
 // Example: userCopy = addFriend("Pierluigi")
-
-let addPierToFriends = {
-  ...user, friends: user.friends.push('Pier')
-}
-console.log({addPierToFriends})
-
 // should return back:
 /**
 {
@@ -110,25 +96,14 @@ console.log({addPierToFriends})
     "Pierluigi"
   ]
 } 
-*/
+*/ 
+
 
 // TODO: Write a function "removeFriend" that receives "friend" as a parameter. friend is a string
 // The function should create a copy of user variable, then then update the array field "friends" on the copy, removing the given friend.
 // Please use the filter method to filter out the given friend
 
 // Example: userCopy = removeFriend("Niko")
-
-let removeLastFriend = {
-  ...user, friends: user.friends.pop(),
-}
-console.log({removeLastFriend})
-
-let removeNiko= {
-  ...user, friends: user.friends.splice(2,1)
-}
-console.log({removeNiko})
-console.log({user})
-
 // should return back
 /**
 {
@@ -142,21 +117,12 @@ console.log({user})
     "Gamze",
   ]
 } 
-*/
+*/ 
+
 
 // TODO: Write a function "toggleAdmin" that receives no parameter
 // The function should create a copy of user variable, then then update the array field "isAdmin" on the copy.
 // In case the user is admin (isAdmin === true) the function should set isAdmin to false
 // In case the user is not admin (isAdmin === false) the function should set isAdmin to true
 
-let isAdmin = {
-  ...user, isAdmin: user.isAdmin = true
-}
-console.log({isAdmin})
-
-// Good luck :D 🐈
-
-let { title, ...rest } = user;
-console.log({ user });
-console.log({ rest });
-
+// Good luck :D
