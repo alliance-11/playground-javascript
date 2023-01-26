e9577bc6ceae4d1cc3afea153a4a317364a366b4
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-26 16:18:21 +0100

    Array - CRUD

---
+++ 23-array-crud.js
@@ -0,0 +1,25 @@
+const cart = [
+  { _id: "f1", name: "Apple", quantity: 2, price: 1 },
+  { _id: "f2", name: "Book", quantity: 1, price: 10 },
+  { _id: "f3", name: "Toaster", quantity: 1, price: 25 },
+  { _id: "f4", name: "TV", quantity: 0, price: 100 },
+];
+// WHOLE ARRAY updates
+// we always copy original "cart" object
+// ADD
+let addToCart = (cart, newThink) => {
+  return [...cart, newThink];
+};
+let newCart = addToCart(cart, { _id: "f6", name: "Lampe", quantity: 2, price:3 });
+console.log(newCart)
+newCart = addToCart(newCart, { _id: "f7", name: "Vase", quantity: 1, price:4 });
+console.log(newCart)
+
+
+// DELETE
+
+// UPDATE
+
+// FILTER
+// - by name
+// - by price range (min, max)
